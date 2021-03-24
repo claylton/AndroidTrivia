c1c78eb4c5c0ce93ae6148da0d30ccf35f36b2ae
Claylton dos Santos Pinto <clayltonsp@hotmail.com>
2021-03-24 16:13:24 -0300

    Navegar através da ação

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -21,7 +21,7 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_title, container, false)
         binding.playButton.setOnClickListener { view: View ->
-            Navigation.findNavController(view)
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
         }
         return  binding.root
 
