ef5132c96463ad759b784f1968cf54fd497f0939
Claylton dos Santos Pinto <clayltonsp@hotmail.com>
2021-03-24 16:21:07 -0300

    Simplificando linhas de código

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -21,10 +21,10 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_title, container, false)
-        binding.playButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
-        }
-        return  binding.root
+        binding.playButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
+        return binding.root
 
     }
 
